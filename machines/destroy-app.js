cfd4333e5097d170133423767545e8014a9d8876
Cody Stoltman <particlebanana@gmail.com>
2015-03-05 09:37:07 -0800

    add destroy app machine

---
+++ machines/destroy-app.js
@@ -0,0 +1,81 @@
+module.exports = {
+
+
+  friendlyName: 'Destroy app',
+
+
+  description: 'Destroys an application.',
+
+
+  extendedDescription: '',
+
+
+  inputs: {
+
+    token: {
+      description: 'An access token for use with a Deis install.',
+      example: '',
+      required: true
+    },
+
+    controller: {
+      description: 'The controller location of your Deis install.',
+      example: 'http://deis.mydeisapp.com',
+      required: true
+    },
+
+    app: {
+      description: 'The uniquely identifiable name for the application.',
+      example: 'myawesomeapp',
+      required: true
+    }
+
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.',
+    },
+
+    notAuthenticated: {
+      description: 'The token provided were invalid.'
+    },
+
+    success: {
+      description: 'Application successfully removed.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    // Dependencies
+    var request = require('request');
+
+    var url = inputs.controller + '/v1/apps/' + inputs.app;
+
+    var headers = {
+      'content-type': 'application/json',
+      'X-Deis-Version': 1,
+      'Authorization': 'token ' + inputs.token
+    };
+
+    // Make the HTTP request
+    request.del({ url: url, form: {}, headers: headers, json: true }, function(err, response, body) {
+      if(err) return exits.error();
+
+      var code = response.statusCode;
+      if(!code) return exits.error();
+      if(code > 499) return exits.error();
+      if(code > 299) return exits.notAuthenticated();
+      return exits.success();
+    });
+  }
+
+};
