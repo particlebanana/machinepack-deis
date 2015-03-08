a8c420da06cd6590fe77953b5684e16f783c3fb0
Cody Stoltman <particlebanana@gmail.com>
2015-03-08 12:51:30 -0700

    add list limits machine

---
+++ machines/list-limits.js
@@ -0,0 +1,92 @@
+module.exports = {
+
+
+  friendlyName: 'List limits',
+
+
+  description: 'Lists resource limits for an application.',
+
+
+  extendedDescription: '',
+
+
+  inputs: {
+
+    token: {
+      description: 'An access token for use with a Deis install.',
+      example: '',
+      required: true
+    },
+
+    controller: {
+      description: 'The controller location of your Deis install.',
+      example: 'http://deis.mydeisapp.com',
+      required: true
+    },
+
+    app: {
+      description: 'The uniquely identifiable name for the application.',
+      example: 'myawesomeapp',
+      required: true
+    }
+
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.',
+    },
+
+    notAuthenticated: {
+      description: 'The token provided was invalid'
+    },
+
+    success: {
+      description: 'Successfully set limits.',
+      example: {
+        memory: '512M',
+        cpu: 'unlimited'
+      }
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+
+    // Dependencies
+    var request = require('request');
+
+    var url = inputs.controller + '/v1/apps/' + inputs.app + '/config';
+
+    var headers = {
+      'content-type': 'application/json',
+      'X-Deis-Version': 1,
+      'Authorization': 'token ' + inputs.token
+    };
+
+    // Make the HTTP request
+    request.get({ url: url, headers: headers, json: true }, function(err, response, body) {
+      if(err) return exits.error();
+
+      var code = response.statusCode;
+      if(!code) return exits.error();
+
+      if(code > 499) return exits.error();
+      if(code > 299) return exits.notAuthenticated();
+
+      var values = {};
+      values.memory = body.memory && body.memory.memory || 'unlimited';
+      values.cpu = body.cpu && body.cpu.cpu || 'unlimited';
+
+      return exits.success(values);
+    });
+
+  }
+
+};
